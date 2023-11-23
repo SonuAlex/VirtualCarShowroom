59bd6addd5c45387b176560ac61782f8a530e74c
SonuAlex <instructable01@gmail.com>
2023-11-23 10:06:23 +0530

    fix

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Review.css'
 
+import {BsArrowLeftShort} from 'react-icons/bs'
+import {BsArrowRightShort} from 'react-icons/bs'
+
 const Review = () => {
   return (
-    <div className=''>
-        
+    <div className='review section'>
+        <div className="secContainer container">
+          <div className="secHeading flex">
+            <h3 className="secTitle">
+              Recent Reviews
+            </h3>
+
+            <div className="navBtns flex">
+              <BsArrowLeftShort className="icon leftIcon"/>
+              <BsArrowRightShort className="icon rightIcon"/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
